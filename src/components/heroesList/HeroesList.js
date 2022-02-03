9f97c3fb1d3d6ddb552a8bc63e8b651083681b82
Konstantin <konstantinokhorzin@gmail.com>
2022-02-03 19:36:13 +0200

    разбив reduser на категории и соединенние их с помощью combineReducers

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -9,13 +9,13 @@ import Spinner from '../spinner/Spinner';
 
 const HeroesList = () => {
     const filteredHeroes = useSelector(state => {
-        if (state.activeFilter === 'all') {
-            return state.heroes;
+        if (state.filters.activeFilter === 'all') {
+            return state.heroes.heroes;
         } else {
-            return state.heroes.filter(item => item.element === state.activeFilter)
+            return state.heroes.heroes.filter(item => item.element === state.filters.activeFilter)
         }
     })
-    const {heroesLoadingStatus} = useSelector(state => state.heroesLoadingStatus);
+    const {heroesLoadingStatus} = useSelector(state => state.heroes.heroesLoadingStatus);
     const dispatch = useDispatch();
     const {request} = useHttp();
 
