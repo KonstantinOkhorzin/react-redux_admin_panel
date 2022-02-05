0ec1cb05656954b1ddc7e9a05e13ef3497fd0133
Konstantin <konstantinokhorzin@gmail.com>
2022-02-05 08:50:06 +0200

    Redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
 export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
@@ -22,6 +29,13 @@ export const heroDeleted = (id) => {
         type: 'HERO_DELETED',
         payload: id
     }
+}
+
+export const fetchFilter = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+        request("http://localhost:3001/filters")
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()))
 }
 
 export const filtersFetching = () => {
@@ -49,6 +63,15 @@ export const activeFilterChanged = (filter) => {
         payload: filter
     }
 }
+
+// export const activeFilterChanged = (filter) => (dispatch) => {
+//     setTimeout(() => {
+//         dispatch({
+//             type: 'ACTIVE_FILTER_CHANGED',
+//             payload: filter
+//         })
+//     }, 1000)
+// }
 
 export const heroCreated = (hero) => {
     return {
